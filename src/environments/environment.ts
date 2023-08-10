1594383988f88953a9e9342af340b33527baaae3
DESKTOP-R6DHVIO\Otmars <ootmar1998@gmail.com>
2023-08-09 22:50:02 -0400

    cambio de backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    backend : 'https://asistencia-back.onrender.com/'
+    // backend : 'https://asistencia-back.onrender.com/'
+    backend : 'https://asistencia-back-production.up.railway.app/'   
 };
