1e0c115e2da1f3286b7326741df0c5fbcfa37a0d
DESKTOP-R6DHVIO\Otmars <ootmar1998@gmail.com>
2023-11-17 10:44:26 -0400

    cambio de enviorament

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // backend : 'https://asistencia-back.onrender.com/'
-    backend : 'https://asistencia-back-production.up.railway.app/'   
+    backend : 'https://asistencia-back.onrender.com/'
+    // backend : 'https://asistencia-back-production.up.railway.app/'   
 };
